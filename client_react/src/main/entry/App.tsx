820b7236f94802ca7168c089d18532ad97a20f33
paxibay <paxibay@yahoo.ca>
2017-01-31 10:05:35 -0500

    refactoring

--- client_react/src/main/entry/App.tsx
+++ client_react/src/main/entry/App.tsx
@@ -9,24 +9,4 @@ export default class App extends React.Component<{}, {}> {
                 return fetch('/api/locations/' + '56c8dd0bdafe64443c11898f')
                     .then((response) => response.json());
             },
-
-            //fetchTickets(origin, destination) {
-            //    return fetch('flights.json')
-            //        .then((response) => response.json());
-            //}
-            // return this.http.get('/api/locations/' + '56c8dd0bdafe64443c11898f')
-        };        console.log(AirCheapAPI.fetchAirports());        return (            <div>                <Navbar />                <div className={contentTop}>                    {this.props.children}                </div>            </div>        );    }}
-
-//let AirCheapAPI = {
-//  fetchAirports() {
-//    return fetch('airports.json')
-//      .then((response) => response.json());
-//  },
-
-//  fetchTickets(origin, destination) {
-//    return fetch('flights.json')
-//      .then((response) => response.json());
-//  }
-//};
-
-//export default AirCheapAPI;
+        };        console.log(AirCheapAPI.fetchAirports());        return (            <div>                <Navbar />                <div className={contentTop}>                    {this.props.children}                </div>            </div>        );    }}
